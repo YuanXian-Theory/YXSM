5b745106cb54cda308624c2b50d86078f25e39b9
Zhenyuan Acharya <yuanxian.philosophy@gmail.com>
2026-05-05 10:47:56 +0800

    Update T64.lean

--- lean/YXTT/T64.lean
+++ lean/YXTT/T64.lean
@@ -1,10 +1,10 @@
 import YXTT.Basic
-import Mathlib.Topology.ContinuousMap
+import Mathlib.Topology.Instances.Circle
 
-/-- 64-dimensional torus T⁶⁴ --/
+/-- 64-dimensional torus T⁶⁴ as the product of 64 circles. -/
 abbrev T64 : Type := Fin 64 → Circle
 
-/-- T64 implements YuanXianType --/
+/-- T64 implements YuanXianType. -/
 instance : YuanXianType T64 where
   self_ref f := f
   is_TCSC _ := True
